131ba80da6f57379f332fcc246d962f26944a485
Dillon Goetz <dgoetzcodes@gmail.com>
2025-02-18 01:38:42 -0500

    removed old code

--- src/config/appwriteConfig.js
+++ src/config/appwriteConfig.js
@@ -11,16 +11,3 @@ const storage = new Storage(client);
 export { client, databases, ID, Query, storage }; // Export the client and databases
 
 
-
-// probably garbage code lol
-// const promise = storage.createFile(
-//   'behavioral',
-//   ID.unique(),
-//   document.getElementById('uploader').files[0]
-// );
-
-// promise.then(function (response) {
-//   console.log(response); // Success
-// }, function (error) {
-//   console.log(error); // Failure
-// });
